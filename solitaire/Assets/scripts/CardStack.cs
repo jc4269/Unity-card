b0643c606e0b861357530bbf0c6ba19643e3f020
Jai Carlton <jai.carlton@gmail.com>
2018-10-04 23:25:57 +1000

    refactored some naming and code. created game deck and player "hand" objects and created a debug script showing transfer of card from game deck to player hand. Doesn't remove cards from game deck yet visually.

---
+++ solitaire/Assets/scripts/CardStack.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStack : MonoBehaviour 
+{
+	List<int> cards;
+
+	public bool isGameDeck = false;
+
+	public bool hasCards{
+		get { return cards != null && cards.Count > 0; }
+	}
+
+	public IEnumerable<int> getCards(){
+		foreach (int i in cards) {
+			yield return i;
+		}
+	}
+
+	public int cardsCount(){
+		if(cards == null){
+			return 0;
+		}
+		//else
+		return cards.Count;
+	}
+
+	public int pop(){
+		//TODO: error checking, bounds checking.
+		int temp = cards [0];
+		cards.RemoveAt (0);
+		return temp;
+	}
+
+	public void push (int index){
+		cards.Add (index);
+	}
+
+	public void CreateDeck(){
+
+		cards.Clear();
+
+		//add unshuffled cards to list (deck)
+		for(int i = 0; i < 52; i++){
+			cards.Add(i);
+			//Debug.Log (i);
+		}
+
+
+		int n = cards.Count; 	//number of cards in deck
+		Debug.Log("n="+n);
+		int k; 						// random value
+		int temp; 					// holds int for swapping
+		while (n > 1) {
+			n--;
+			k = Random.Range(0, n+1);
+			temp = cards [k];
+			cards [k] = cards [n];
+			cards [n] = temp; 
+		}
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+		cards = new List<int>();
+		if (isGameDeck) {
+			CreateDeck ();
+		}
+	}
+
+}
---
+++ solitaire/Assets/scripts/CardStackView.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CardStack))]
+public class CardStackView : MonoBehaviour 
+{
+	CardStack cardStack;
+	List<int> fetchedCards;
+	int lastCount;
+
+	public Vector3 startPosition;
+	public float cardOffset;
+	public GameObject cardPrefab;
+
+
+	void Start(){
+		cardStack = GetComponent<CardStack> ();
+		fetchedCards = new List<int>();
+		showCards ();
+		lastCount = cardStack.cardsCount();
+	}
+
+	void Update(){
+		if(lastCount != cardStack.cardsCount()){
+			showCards ();
+			lastCount = cardStack.cardsCount();
+		}
+	}
+
+	//show a deck of cards setting position with offset and showing face up, also dealing with render order.
+	void showCards(){
+		int cardCount = 0;
+		float co;
+		if (cardStack.hasCards) {
+			foreach (int i in cardStack.getCards()) {
+				co = cardOffset * cardCount;
+				Vector3 temp = startPosition + (new Vector3 (co, 0f, 0f));
+				addCard (temp, i, cardCount);
+				cardCount++;
+			}
+		}
+	}
+
+	void addCard(Vector3 position, int cardIndex, int renderIndex){
+
+		if (fetchedCards.Contains (cardIndex)) {
+			return;
+		}
+
+		GameObject cardCopy = (GameObject)Instantiate (cardPrefab);
+		cardCopy.transform.position = position;
+		CardModel cardModel = cardCopy.GetComponent<CardModel> ();
+		cardModel.cardIndex = cardIndex;
+		cardModel.toggleFace (true);
+
+		SpriteRenderer spriteRenderer = cardCopy.GetComponent<SpriteRenderer> ();
+		spriteRenderer.sortingOrder = renderIndex; //left to right put down first to last.
+		//spriteRenderer.sortingOrder = 51 - renderIndex; //right to left put down first to last.
+
+		//keep track of added cards
+		fetchedCards.Add(cardIndex);
+	}
+}

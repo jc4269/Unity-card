3b31f25cad610933cbb0e9790acaaa9cd53ec96c
Jai Carlton <jai.carlton@gmail.com>
2018-10-04 11:25:43 +1000

    Created card model with back and faces and functionality to show both. Created a debug script for card model to test its working.

---
+++ solitaire/Assets/scripts/CardModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour 
+{
+	SpriteRenderer spriteRenderer;
+
+	public Sprite[] faces;
+	public Sprite cardback;
+	public int cardIndex; // e.g. faces[cardIndex]
+
+	public void toggleFace(bool showFace){
+		if (showFace) {
+			spriteRenderer.sprite = faces[cardIndex];
+		} else {
+			spriteRenderer.sprite = cardback;
+		}
+	}
+
+	void Awake(){
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+}
---
+++ solitaire/Assets/scripts/DebugChangeCard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour {
+	CardModel cardModel;
+	int cardIndex = 0;
+
+
+	public GameObject card;
+
+
+	void Awake () {
+		cardModel = card.GetComponent<CardModel>();
+	}
+	
+	void OnGUI(){
+
+		if(GUI.Button(new Rect(10,10,100,20),"Hit me!")){
+			if (cardIndex >= cardModel.faces.Length) {
+				cardIndex = 0;
+				cardModel.toggleFace (false);
+			} else {
+				cardModel.cardIndex = cardIndex++;
+				cardModel.toggleFace (true);
+			}
+
+
+			//cardIndex++;
+		}
+
+	}
+}

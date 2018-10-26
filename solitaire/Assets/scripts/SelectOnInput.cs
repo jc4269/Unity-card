91328a54063087debc627b72b80df481dd42cacd
Jai Carlton <jai.carlton@gmail.com>
2018-10-26 18:23:32 +1000

    Added back to menu buttons. Still need to deal with cleaning up before going back.

--- solitaire/Assets/scripts/SelectOnInput.cs
+++ solitaire/Assets/scripts/SelectOnInput.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 //this is for keyboard or gamepad to be used on panel parent of some buttons
 //buttons in unity editor have a navagation setting in a default compontent and that has them connected already based on hierarchy i guess
+//	does require a already selected object
 public class SelectOnInput : MonoBehaviour {
 
 	public EventSystem eventSystem;
@@ -19,7 +20,7 @@ public class SelectOnInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false) {//any movement up/down	
-			eventSystem.SetSelectedGameObject(selectedObject) ;
+			eventSystem.SetSelectedGameObject(selectedObject) ; // i
 			buttonSelected = true;
 		}
 	}

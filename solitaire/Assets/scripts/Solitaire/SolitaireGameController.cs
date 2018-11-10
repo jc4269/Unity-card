770679b412de26540964d6dc94533afd2f68a920
Jai Carlton <jai.carlton@gmail.com>
2018-11-10 19:17:33 +1000

    implemented drag and drop with one card with valid rules. added only king can go into empty column. implemented column move.

--- solitaire/Assets/scripts/Solitaire/SolitaireGameController.cs
+++ solitaire/Assets/scripts/Solitaire/SolitaireGameController.cs
@@ -178,6 +178,30 @@ public class SolitaireGameController : MonoBehaviour, IGameController {
             Debug.Log("templist.count="+tempList.Count);
             return tempList; // empty, dont do anything.
         }
+        if (cardStackViewOfCardBeingMoved.tag == "Column")
+        {
+            //if card is face down, can't be selected so return nothing
+            if(gcClicked.GetComponent<CardModelNew>().faceUp == false){
+                Debug.Log("card is face down, not valid");
+                return tempList; //should be empty;
+            }
+            //Debug.Log("is a column");
+
+
+            //check if card to pile end is valid (descending rank and alternating card colour)
+            if (isCardToColumnEndInValidOrder(gcClicked))
+            {
+                cardsToMoveAsColumn = getCardsFromSelectedToEndOfColumn(gcClicked);
+                Debug.Log("card till end is valid");
+                return cardsToMoveAsColumn;
+
+            }
+            else
+            {
+                Debug.Log("NOT card till end is valid");
+            }
+            return tempList;
+        }
         tempList.Add(gcClicked);
         return tempList;
     }
@@ -185,6 +209,117 @@ public class SolitaireGameController : MonoBehaviour, IGameController {
     public void onMouseUpGameLogic(List<GameObject> gcDraggedList, Vector3 mousePosition, CardStackViewNew cardStackViewOfCardBeingMoved, int sizeOfColumnBeingMoved)
     {
         Debug.Log("onMouseUp");
+        if (gcDraggedList.Count > 0)
+        {
+            GameObject selectedCardDragged = gcDraggedList[0]; // first card in list is the base card of column being dragged.
+            //hit test to see which column or row its over.
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+            //LayerMask layerMask;
+            //layerMask = 1 << LayerMask.NameToLayer ("CardStack"); // only check for collisions with cards
+            //hit = raycastFirstHitWithLayerMask(ray, layerMask);
+            //Debug.Log("CardStackLayerNumber:"+LayerMask.GetMask("CardStack"));
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("CardStack")))
+            {
+                Debug.Log("stackname:" + hit.collider.gameObject.name);
+                Debug.Log("stacktag:" + hit.collider.gameObject.tag);
+                GameObject cardStackHit = hit.collider.gameObject;
+
+                CardStackViewNew cardStackHitCardStackView = cardStackHit.GetComponent<CardStackViewNew>();
+                CardStackNew cardStackHitCardStack = cardStackHit.GetComponent<CardStackNew>();
+
+                CardModelNew cardModelNew = selectedCardDragged.GetComponent<CardModelNew>();
+                //Debug ("card suit="+getCardSuitFromIndex( cm.cardIndex) + ", card rank=" + getCardRankFromIndex( cm.cardIndex));
+                GameObject cardStackIn = cardModelNew.StackIn;
+                CardStackViewNew cardStackInCardStackViewNew = cardStackIn.GetComponent<CardStackViewNew>();
+                CardStackNew cardStackInCardStackNew = cardStackIn.GetComponent<CardStackNew>();
+                //Debug.Log("getmouseup.name="+cmzoneIn);
+                //FreeCellGameController freeCellGC = GetComponent<FreeCellGameController> ();
+                if (checkIfValidDrop(cardStackHit, selectedCardDragged, sizeOfColumnBeingMoved))
+                {
+                    Debug.Log("Valid Drop and gcdraggedlist.count=" + gcDraggedList.Count);
+                    //valid so update all cards in column being dragged.
+                    for (int i = 0; i < gcDraggedList.Count; i++)
+                    {
+                        cardModelNew = gcDraggedList[i].GetComponent<CardModelNew>();
+                        //cardStackInCardStackViewNew.removeCardFromFetchedWithIndex(cm.cardIndex);
+                        cardStackInCardStackNew.Cards.Remove(gcDraggedList[i]);
+                        //cm.zoneIn = cardStackHit; //updating to cards new zone
+                        cardStackHitCardStack.Push(gcDraggedList[i]);
+                    }
+                    //update cardStack that Card is going into, the hit stack
+                    if (cardStackHit.tag == "PileStackRow")
+                    {
+                        updatePileStackView(cardStackHit);
+                    }
+                    else if (cardStackHit.tag == "DrawnPileColumn")
+                    {
+                        DrawnPileColumUpdateView();
+                    }
+                    else
+                    {
+                        cardStackHitCardStackView.UpdateStackView();
+                    }
+
+                    //update cardStack that Card coming from, last zone card was in.
+                    if (cardStackIn.tag == "PileStackRow")
+                    {
+                        updatePileStackView(cardStackIn);
+                    }
+                    else if (cardStackIn.tag == "DrawnPileColumn")
+                    {
+                        DrawnPileColumUpdateView();
+                    }
+                    else
+                    {
+                        cardStackInCardStackViewNew.UpdateStackView();
+                    }
+
+
+                    //check win condition after every valid action.
+
+                    bool didWinGame = false;//checkWinGame();
+                    Debug.Log("win=" + didWinGame);
+                    if (didWinGame)
+                    {
+                        feedBackText.text = "YOU WON!!!   Play Again?";
+                    }
+
+                }
+                else
+                {
+                    //snap back to card stack if not valid
+                    if (cardStackIn.tag == "PileStackRow")
+                    {
+                        updatePileStackView(cardStackIn);
+                    }
+                    else if (cardStackIn.tag == "DrawnPileColumn")
+                    {
+                        DrawnPileColumUpdateView();
+                    }
+                    else
+                    {
+                        cardStackInCardStackViewNew.UpdateStackView();
+                    }
+                }
+            }
+            else
+            {
+                //didn't drop onto stack, so snap back
+                if (cardStackViewOfCardBeingMoved.gameObject.tag == "PileStackRow")
+                {
+                    updatePileStackView(cardStackViewOfCardBeingMoved.gameObject);
+                }
+                else if (cardStackViewOfCardBeingMoved.gameObject.tag == "DrawnPileColumn")
+                {
+                    DrawnPileColumUpdateView();
+                }
+                else
+                {
+                    cardStackViewOfCardBeingMoved.UpdateStackView();
+                }
+            }
+        }
     }
 // --- END Drag and Drop functions
 
@@ -354,5 +489,302 @@ public class SolitaireGameController : MonoBehaviour, IGameController {
             spriteRenderer.sortingOrder = index; //left to right put down first to last.
         }
         cardViewNew.UpdateSprite();
+    }
+
+    private List<GameObject> getCardsFromSelectedToEndOfColumn(GameObject card)
+    {
+        CardStackViewNew columnCardStackInCardStackViewNew = card.GetComponent<CardModelNew>().StackIn.GetComponent<CardStackViewNew>();
+        CardStackNew columnCardStackInCardStackNew = card.GetComponent<CardModelNew>().StackIn.GetComponent<CardStackNew>();
+        //go through fetched list to find where selected card is then do check from there
+        bool foundSelectedCard = false;
+        int selectedCardIndex = card.GetComponent<CardModelNew>().Index;
+        List<GameObject> temp = new List<GameObject>();
+
+        foreach (GameObject c in columnCardStackInCardStackNew.Cards)
+        {
+
+            //find selected card
+            if (!foundSelectedCard)
+            {
+                if (c.GetComponent<CardModelNew>().Index == selectedCardIndex)
+                {
+                    foundSelectedCard = true;
+                    temp.Add(c);
+                }
+            }
+            else
+            { // start checking for valid column to move all card down from selected card
+                temp.Add(c);
+            }
+
+
+        }
+
+
+
+        return temp;
+    }
+
+    //check if card to pile end is valid to move as a column (descending rank and alternating card colour)
+    public bool isCardToColumnEndInValidOrder(GameObject card)
+    {
+        CardStackViewNew columnCardStackInCardStackViewNew = card.GetComponent<CardModelNew>().StackIn.GetComponent<CardStackViewNew>();
+        CardStackNew columnCardStackInCardStackNew = card.GetComponent<CardModelNew>().StackIn.GetComponent<CardStackNew>();
+        //go through fetched list to find where selected card is then do check from there
+        bool foundSelectedCard = false;
+        int previousCardIndex = -1;
+        int selectedCardIndex = card.GetComponent<CardModelNew>().Index;
+        foreach (GameObject c in columnCardStackInCardStackNew.Cards)
+        {
+
+            //find selected card
+            if (!foundSelectedCard)
+            {
+                if (c.GetComponent<CardModelNew>().Index == selectedCardIndex)
+                {
+                    foundSelectedCard = true;
+                }
+            }
+            else
+            { // start checking for valid column to move all card down from selected card
+                if (!isCardValidToBeOnTopOfAnotherCardInColumn(previousCardIndex, c.GetComponent<CardModelNew>().Index))
+                {
+                    return false;
+                }
+            }
+            previousCardIndex = c.GetComponent<CardModelNew>().Index;
+
+        }
+        return true;
+    }
+
+    //assumes there is a previous card to check. still need to check if previous card is there.
+    bool isCardValidToBeOnTopOfAnotherCardInColumn(int previousCardIndex, int currentCardIndex)
+    {
+        //if current card rank is one less than previous card and suit of current card is opposite colour to previous card,
+        int currentCardRank = getCardRankFromIndex(currentCardIndex);
+        int currentCardSuit = getCardSuitFromIndex(currentCardIndex);
+        int previousCardRank = getCardRankFromIndex(previousCardIndex);
+        int previousCardSuit = getCardSuitFromIndex(previousCardIndex);
+
+        if (currentCardSuit < 2 && previousCardSuit >= 2 || currentCardSuit >= 2 && previousCardSuit < 2)
+        { //make sure suit is different colour
+            if (currentCardRank == previousCardRank - 1)
+            { // card needs to be one less than zone card
+              //then valid, add card to column.
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printCardSuitRankFromIndex(int cardIndex)
+    {
+
+        Debug.Log("Card Index=" + cardIndex + ", Card Suit=" + getCardSuitFromIndex(cardIndex) + ", Card Rank=" + getCardRankFromIndex(cardIndex));
+
+    }
+    //0 - hearts
+    //1 - diamonds
+    //2 - clubs
+    //3 - spades
+    public int getCardSuitFromIndex(int cardIndex)
+    {
+        return cardIndex / 13; // due to interger division, decimals will get trunicated resulting in 0-3 as return.
+    }
+
+    public int getCardRankFromIndex(int cardIndex)
+    {
+        return cardIndex % 13;
+
+    }
+
+    //zone is the column or row where card was dropped on.
+    //c is card that was dropped.
+    public bool checkIfValidDrop(GameObject zone, GameObject c, int sizeOfColumnBeingMoved)
+    {
+        //Debug.Log ("checkIfValidDrop in");
+        //Debug.Log ("zone.tag ="+ zone.tag);
+
+
+        //get rank and suit of card
+        int cardIndex = c.GetComponent<CardModelNew>().Index;
+        int cardRank = getCardRankFromIndex(cardIndex);
+        int cardSuit = getCardSuitFromIndex(cardIndex);
+
+        //get rank and suit of last card in zone.
+        CardStackViewNew lastZoneCardStackView = zone.GetComponent<CardStackViewNew>();
+        CardStackNew lastZoneCardStackNew = zone.GetComponent<CardStackNew>();
+        int zoneLastCardIndex = -1;
+        int zoneLastCardRank = -1;
+        int zoneLastCardSuit = -1;
+        if (lastZoneCardStackNew.Cards.Count > 0)
+        {
+            zoneLastCardIndex = lastZoneCardStackNew.Cards[lastZoneCardStackNew.Cards.Count - 1].GetComponent<CardModelNew>().Index;
+            zoneLastCardRank = getCardRankFromIndex(zoneLastCardIndex);
+            zoneLastCardSuit = getCardSuitFromIndex(zoneLastCardIndex);
+        }
+        //if zone is column
+        if (zone.tag == "Column")
+        {
+            Debug.Log("dorpped on Column");
+            //            Debug.Log ("cardSuit="+cardSuit);
+            //            Debug.Log ("cardRank="+cardRank);
+            //            Debug.Log ("zoneLastCardSuit="+zoneLastCardSuit);
+            //            Debug.Log ("zoneLastCardRank="+zoneLastCardRank);
+
+
+            // if no cards on column, so add card - > value.
+            if (zoneLastCardIndex == -1)
+            {
+                Debug.Log("is empty");
+                // TODO: allow only kings to go into empty zones.
+                if (cardRank == 12) // king
+                { 
+                    return true;
+                }
+            }
+            //if card rank is one less than zone last card and suit of card is opposite colour to zone last card,
+            if (cardSuit < 2 && zoneLastCardSuit >= 2 || cardSuit >= 2 && zoneLastCardSuit < 2)
+            { //make sure suit is different colour
+                if (cardRank == zoneLastCardRank - 1)
+                { // card needs to be one less than zone card
+                    //then valid, add card to column.
+                    return true;
+                }
+            }
+
+        }
+
+        //if zone is pilestackrow
+        if (zone.tag == "PileStackRow")
+        {
+            //Debug.Log ("dorpped on PileStackRow");
+
+            int[] piles = suitPileStackVisibleView(lastZoneCardStackNew);
+            //if card is 1 rank higher than card in its suit pile. (no cards means pile needs an ace)
+            // ace case (lowest card): since empty is -1, +1 = 0 therefore ace handled
+            // king case (highest card): since only one deck and only one copy of each card, there shouldn't be anything higher so should be handled.
+            if (piles[cardSuit] + 1 == cardRank)
+            {
+                return true;
+                //lastZoneCardStackView.addCardToFetchedWithIndexAndCard (cardIndex, c);
+            }
+            //update pile view
+            //updatePileStackView(lastZoneCardStackView);
+            //return true;
+        }
+
+        return false;
+    }
+
+    //return an array of four ints representing the the suit piles with highest rank card on the top of the pile (hearts=0, diamonds=1, clubs=2, spades=3)
+    int[] suitPileStackVisibleView(CardStackNew cardStackNew)
+    {
+        int[] piles = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            piles[i] = -1; //-1 means there is nothing in the corresponding suit pile
+        }
+        foreach (GameObject card in cardStackNew.Cards)
+        {
+            int cardIndex = card.GetComponent<CardModelNew>().Index;
+            int cardSuit = getCardSuitFromIndex(cardIndex);
+            int cardRank = getCardRankFromIndex(cardIndex);
+            //looking for highest rank card for each suit.
+            if (piles[cardSuit] < cardRank)
+            {
+                piles[cardSuit] = cardRank;
+            }
+        }
+
+        return piles;
+    }
+
+
+    //custom update function for specific implementation pilestackview (a cardstack/cardstackview object)
+    void updatePileStackView(GameObject pileStack)
+    {
+        int i = 0;
+        //GameObject card = null;
+        SpriteRenderer spriteRenderer;
+        Vector3 temp;
+        float co;
+        CardStackNew pileStackCardStackNew = PileStackRow.GetComponent<CardStackNew>();
+        CardStackViewNew pileStackCardStackViewNew = PileStackRow.GetComponent<CardStackViewNew>();
+        // stores the entries at the top of each suit pile (hearts=0, diamonds=1, clubs=2, spades=3)
+        //List<KeyValuePair<int, CardView>> suitPile = new List<KeyValuePair<int, CardView>>();
+        List<GameObject> suitPiles = new List<GameObject>();
+        for (int j = 0; j < 4; j++)
+        {
+            suitPiles.Add(null);
+        }
+        //        suitPile.Add(null);
+        //        suitPile.Add(null);
+        //        suitPile.Add(null);
+
+        int cardIndex;
+        int cardSuit;
+        int cardRank;
+        int suitPileRank;
+        foreach (GameObject card in pileStackCardStackNew.Cards)
+        {
+
+            card.GetComponent<BoxCollider>().enabled = false; // set all cards to disabled, will set top of piles back after
+            cardIndex = card.GetComponent<CardModelNew>().Index;
+            cardSuit = getCardSuitFromIndex(cardIndex);
+            cardRank = getCardRankFromIndex(cardIndex);
+            //Debug.Log("value of suitpile[cardindex]="+suitPiles [cardindex]);
+            //Debug.Log("entryCardSuit="+cardSuit+", entryCardRank="+cardRank);
+            if (suitPiles[cardSuit] == null)
+            { // empty
+                suitPiles[cardSuit] = card;
+            }
+            else
+            {
+                suitPileRank = getCardRankFromIndex(suitPiles[cardSuit].GetComponent<CardModelNew>().Index);
+                //Debug.Log("suitCardRank="+suitPileRank);
+                //if card of same suit is higher than previous recorded, update suitpile column with current card entry.
+                if (suitPileRank < cardRank)
+                {
+                    suitPiles[cardSuit] = card;
+                }
+            }
+
+
+
+
+
+            spriteRenderer = card.GetComponent<SpriteRenderer>();
+            //Vector3 temp = c.transform.position;
+            co = -1.6f * cardSuit;
+
+
+            temp = pileStackCardStackViewNew.StartPosition + pileStackCardStackViewNew.OffsetPositionWithDirection(co);
+
+            card.transform.position = temp;
+            spriteRenderer.sortingOrder = cardRank;
+
+            i++;
+        }
+
+
+        //set cardstack object collider size to encompass all cards.
+        //TODO:see if this dynamicism is still needed?
+
+        //set pile top cards to enabled.
+        for (int j = 0; j < 4; j++)
+        {
+
+            if (suitPiles[j] != null)
+            {
+                Debug.Log("j=" + j + ", rank=" + getCardRankFromIndex(suitPiles[j].GetComponent<CardModelNew>().Index));
+                suitPiles[j].GetComponent<BoxCollider>().enabled = true;
+            }
+            else
+            {
+                Debug.Log("j=" + j + " is empty.");
+            }
+        }
     }
 }
